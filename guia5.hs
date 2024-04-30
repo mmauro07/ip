f22b3fbfc4d0a7a55531f00dde02ca1230bd8105
mmauro07 <167775402+mmauro07@users.noreply.github.com>
2024-04-29 22:06:34 -0300

    Update guia5.hs

--- guia5.hs
+++ guia5.hs
@@ -81,3 +81,63 @@ multiplosDeN n (x:xs)
 ordenar :: [Integer] -> [Integer]
 ordenar [] = []
 ordenar (x:xs) = ordenar (quitar (maximo (x:xs))) ++ [maximo (x:xs)]
+-----ejercicioCuatro-----
+sacarEspaciosRepetidos :: [Char] -> [Char]
+sacarEspaciosRepetidos [] = []
+sacarEspaciosRepetidos [x] = [x]
+sacarEspaciosRepetidos (x:y:xs) | x==y && x==' ' = sacarEspaciosRepetidos (y:xs)
+                                | otherwise =  x:sacarEspaciosRepetidos (y:xs) 
+contarPalabras :: [Char] -> Int
+contarPalabras xs = contarEspacios (sacarEspaciosIniFin (sacarEspaciosRepetidos xs)) + 1
+sacarEspaciosIniFin :: [Char] -> [Char]
+sacarEspaciosIniFin [] = []
+sacarEspaciosIniFin (x:xs) | x==' ' = sacarEspacioFin xs
+                           | otherwise = x:sacarEspacioFin xs
+sacarEspacioFin :: [Char] -> [Char]
+sacarEspacioFin [] = []
+sacarEspacioFin [x] | x==' ' = []
+                       | otherwise = [x] 
+sacarEspacioFin (x:xs) = x:sacarEspacioFin xs
+contarEspacios :: [Char] -> Int
+contarEspacios [] = 0
+contarEspacios (x:xs) | x==' '= 1 + contarEspacios xs
+                      | otherwise = contarEspacios xs
+palabras :: [Char] -> [[Char]]
+palabras xs = palabrasAux (sacarEspaciosIniFin (sacarEspaciosRepetidos xs))
+palabrasAux :: [Char] -> [[Char]]
+palabrasAux [] = []
+palabrasAux (x:xs) = primeraPalabra (x:xs):palabrasAux (sacarPrimeraPalabra (x:xs))
+primeraPalabra :: [Char] -> [Char]
+primeraPalabra [] = []
+primeraPalabra (x:xs) | x == ' ' = []
+                      | otherwise = x:primeraPalabra xs                    
+sacarPrimeraPalabra :: [Char] -> [Char]
+sacarPrimeraPalabra [] = []
+sacarPrimeraPalabra (x:xs) | x == ' ' = xs
+                           | otherwise = sacarPrimeraPalabra xs  
+palabraMasLarga :: [Char] -> [Char]
+palabraMasLarga xs = palabraMasLargaAux (sacarEspaciosIniFin (sacarEspaciosRepetidos xs))
+palabraMasLargaAux :: [Char] -> [Char]
+palabraMasLargaAux xs | sacarPrimeraPalabra xs == [] = primeraPalabra xs
+                      | length (primeraPalabra xs) > length (palabraMasLargaAux (sacarPrimeraPalabra xs)) = primeraPalabra xs
+                      | otherwise = palabraMasLargaAux (sacarPrimeraPalabra xs)                      
+aplanar :: [[Char]] -> [Char]
+aplanar [] = []
+aplanar (xs:xss) = xs ++ aplanar xss
+-----ejercicioCinco-----
+sumaAcumulada :: (Num t) => [t] -> [t]
+sumaAcumulada [] = []
+sumaAcumulada xs = sumaAcumulada' xs 0
+    where
+        sumaAcumulada' [] _ = []
+        sumaAcumulada' (x:xs) acc = (x + acc) : sumaAcumulada' xs (x + acc)
+descomponerEnPrimos :: [Integer] -> [[Integer]]
+descomponerEnPrimos [] = []
+descomponerEnPrimos (x:xs) = obtenerFactoresPrimos x : descomponerEnPrimos xs
+obtenerFactoresPrimos :: Integer -> [Integer]
+obtenerFactoresPrimos n = factorizar n 2
+    where
+        factorizar n divisor
+            | n <= 1 = []
+            | n `mod` divisor == 0 = divisor : factorizar (n `div` divisor) divisor
+            | otherwise = factorizar n (divisor + 1)
