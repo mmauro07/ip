1581ad41f7c279cfd682d266f4d5cd48295bf08f
mmauro07 <167775402+mmauro07@users.noreply.github.com>
2024-05-15 10:21:00 -0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+#ejercicio 1.1
+def imprimir_hola_mundo():
+    print ("hola mundo")
+
+#ejercicio 1.5
+def perimetro(x:int) -> int:
+    x = 2 * 3.14
+    print ("el perímetro es igual a dos por pi")
+    return x
+
+#ejercicio 2.5
+def es_multiplo_de(n:int, m:int) -> Bool:
+    if 
